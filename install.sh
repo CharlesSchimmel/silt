0e400fff4223cb24a2155777a969646239e51c7f
charlesSchimmel <charles.schimmel@gmail.com>
2015-10-10 13:25:57 -0500

    better management of dependencies

--- install.sh
+++ install.sh
@@ -5,11 +5,18 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 else
     echo "Checking/Installing Pip3"
-    apt-get install pip3
-    echo "Checking/Installing Requests"
+    if ! pip3 >/dev/null 2>&1; then
+        echo "pip3 not found, attempting to install:"
+        if apt >/dev/null 2>&1; then
+            apt-get install python3.4 python3-pip
+        else
+            echo "apt-get currently the only supported package manager. Please install pip3 seperately."
+        fi
+    fi
+    echo "Checking/Installing Requests via pip3"
     pip3 install requests
-    cp silt.py /usr/bin/silt
-    cp silt.ini $HOME/.config/silt.ini
+    cp -p silt.py /usr/bin/silt
+    cp -p silt.ini $HOME/.config/silt.ini
     echo "Done. Enter 'silt' to run."
     echo "Config located at $HOME/.config/silt.ini"
     exit 1
