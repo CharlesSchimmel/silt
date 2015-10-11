fe23177e9590b900d38662117c7f5be0053802eb
charlesSchimmel <charles.schimmel@gmail.com>
2015-10-10 21:29:04 -0500

    ..

--- install.sh
+++ install.sh
@@ -1,26 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+
+CONFIG=$HOME/.config/silt.ini
 
 if [ "$(id -u)" != "0" ]; then
     echo "You must be root to install this script." 1>&2
     exit 1
 else
-    echo "This will install 'silt.' \n[ENTER] to confirm"
+    echo "This will install 'silt.'"
+    echo "[ENTER] to confirm"
     read confirm
-    if ! [ $confirm == 'n' ]; then
     echo "Checking/Installing Pip3"
         if ! pip3 >/dev/null 2>&1; then
             echo "pip3 not found, attempting to install:"
             if apt >/dev/null 2>&1; then
                 apt-get install python3.4 python3-pip
             else
-                echo "apt-get currently the only supported package manager. Please install pip3 seperately."
+                echo "apt currently the only supported package manager. Please install pip3 seperately."
             fi
         fi
         echo "Checking/Installing Requests via pip3"
         pip3 install requests
         cp -p silt.py /usr/bin/silt
-        cp -p silt.ini $HOME/.config/silt.ini
+        if ! [ -e $CONFIG ]; then
+            echo "Copying config..."
+            cp -p silt.ini $CONFIG
+        fi
         echo "Done. Enter 'silt' to run."
         echo "Config located at $HOME/.config/silt.ini"
-    fi
 fi
--- silt.py
+++ silt.py
@@ -2,14 +2,25 @@
 
 import requests,json,random
 from sys import argv
-import os
+from os import getenv
 import configparser
 
 
-"""
-pulling from discogs whether or not the user exists
-"""
+def help():
+    """
+    argument help statement
+    """
+    print('\nsilt: discogs recommendations for the command line why not')
+    print('Usage:')
+    # print("silt [-u 'username'] [-c 'non-default config']")
+    print("silt [-r #] [-u 'username']")
+    print("-r: number of recommendations. Default = 1")
+    print("-u: username")
+
 def discogsPull(user):
+    """
+    pulling from discogs whether or not the user exists
+    """
     user_agent = {'User-agent':'silt/0.0'}
     discogsURL = 'https://api.discogs.com/users/{}/collection/folders/0/releases'.format(user)
     r = requests.get(discogsURL, headers = user_agent)
@@ -25,39 +36,95 @@ def discogsPull(user):
         libraryKeys = list(library)
         return library, libraryKeys
 
-"""
-the user must be valid, parsing through data and populating a list of their collection
-if we were successfully able to pull all of this information, open a new config and write that username to file 
-"""
-def getRandom(library,libraryKeys):
+def getRandom(recAmt,library,libraryKeys):
+    """
+    the user must be valid, parsing through data and populating a list of their collection
+    if we were successfully able to pull all of this information, open a new config and write that username to file 
+    """
     random.seed()
-    listenTo = libraryKeys[random.randrange(len(library))]
+    listenTo = []
+    for ct in range(recAmt):
+        tempRec = libraryKeys[random.randrange(len(library))]
+        tempStr = tempRec+' by '+library[tempRec]
+        if tempStr not in listenTo:
+            listenTo.append(tempStr)
+        else:
+            ct += 1
     return listenTo
 
-def popLibrary(user):
+def popLibrary(user,recAmt):
+    """
+    does the heavy lifting, interprates discogsPull and prints the recommendations.
+    """
     if discogsPull(user) != False:
         library, libraryKeys = discogsPull(user)
-        listenTo = getRandom(library,libraryKeys)
-        print('\nYou should listen to:\n'+listenTo,'by',library[listenTo])
-        no = input('\n\nAnother?')
-        while no.lower() != 'n' and no.lower() != 'no':
-            listenTo = libraryKeys[random.randrange(len(library))]
-            print('\nHow about...\n'+listenTo,'by',library[listenTo])
-            no = input('\n\nAnother?')
+        listenTo = getRandom(recAmt,library,libraryKeys)
+        print('\nYou should listen to:\n')
+        for rec in listenTo:
+            print(rec)
+        # for ct in range(0,recAmt):
+        #     print(listenTo,'by',library[listenTo])
+        #     listenTo = getRandom(library,libraryKeys)
+
     else:
-        print('Invalid User.')
+        print('Invalid User "{}".'.format(user))
         user = input('Enter discogs username:')
         popLibrary(user)
 
+# def configParse(toParse):
+    
+
+
+home = getenv("HOME") # defining home folder
+config = configparser.ConfigParser() #initializing config parser object
+config.read('{}/.config/silt.ini'.format(home)) #sending default config file to config parser object
+
 """
-checking for arguments
+main-ish
+interprates arguments and decides what to do depending on what arguments are provided
+too messy, I'd like to clean this up
 """
 if len(argv) > 1:
-    if '-u' in argv and argv.index('-u') + 1 < len(argv):
-        user = argv[argv.index('-u') + 1]
-    elif not argv.index('-u') + 1 < len(argv):
-        user = input('Enter discogs username:')
-else: 
-    user = input('Enter discogs username:')
+    if '-u' in argv:
+        if argv.index('-u') + 1 < len(argv):
+            user = argv[argv.index('-u') + 1]
+        elif not argv.index('-u') + 1 < len(argv):
+            print('Incorrect formatting for "-u" argument. We need something after it.')
+            user = input('Enter discogs username:')
+    elif '-u' not in argv:
+        if 'user' in config['default']:
+            user = config['default']['user']
+        else:
+            user = input('Enter discogs username:')
 
-popLibrary(user)
+    if '-r' in argv:
+        if argv.index('-r') + 1 < len(argv):
+            try:
+                int(argv[argv.index('-r') + 1])
+                recAmt = int(argv[argv.index('-r') + 1])
+            except (TypeError,ValueError):
+                print('Incorrect formatting for "-r" argument. We need an integer after it.')
+                help()
+                recAmt = 0
+        else:
+            help()
+    else:
+        if 'user' in config['default']:
+            user = config['default']['user']
+        else:
+            user = input('Enter discogs username:')
+        help()
+
+else: 
+    recAmt = 1
+    if 'recAmt' in config['default']:
+        recAmt = config['default']
+    else:
+        recAmt = 1
+
+    if 'user' in config['default']:
+        user = config['default']['user']
+    else:
+        user = input('Enter discogs username:')
+
+popLibrary(user,recAmt)
