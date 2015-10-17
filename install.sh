97cea98eb3c182c1b53f1b5e8b5087924ae02ff4
charlesSchimmel <charles.schimmel@gmail.com>
2015-10-17 18:33:40 -0500

    added local storage of library after first run to incrase response time

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-CONFIG=$HOME/.config/silt.ini
+APP=$HOME/.silt/
+CONFIG=$APP/silt.ini
+BINLOC=/usr/local/bin/silt
 
 if [ "$(id -u)" != "0" ]; then
     echo "You must be root to install this script." 1>&2
     exit 1
 else
-    echo "This will install 'silt.'"
+    echo "This will install 'silt' and its dependency 'requests' via pip3.'"
     echo "[ENTER] to confirm"
     read confirm
     echo "Checking/Installing Pip3"
@@ -15,16 +17,32 @@ else
             if apt >/dev/null 2>&1; then
                 apt-get install python3.4 python3-pip
             else
-                echo "apt currently the only supported package manager. Please install pip3 seperately."
+                echo "apt currently the only supported package manager. Please install pip3 and requests seperately."
             fi
         fi
         echo "Checking/Installing Requests via pip3"
         pip3 install requests
-        cp -p silt.py /usr/local/bin/silt
+
+        #check if app location exists; only make it if it exists
+        if ! [ -e $APP ]; then
+            mkdir -m 777 $APP
+        fi
+
+        #cp will overwrite
+        cp -p silt.py $APP/silt.py
+
+        #if there's already something in the bin location, delete it
+        if [ -e $BINLOC ]; then
+            rm $BINLOC
+        fi
+        #make a softlink from the app location to the bin location
+        ln -s $APP/silt.py /usr/local/bin/silt
+
         if ! [ -e $CONFIG ]; then
             echo "Copying config..."
             cp -p silt.ini $CONFIG
         fi
+
         echo "Done. Enter 'silt' to run."
-        echo "Config located at $HOME/.config/silt.ini"
+        echo "Config located at $HOME/.silt/silt.ini"
 fi
--- silt.py
+++ silt.py
@@ -3,6 +3,7 @@
 import requests,json,random
 from sys import argv
 from os import getenv
+from os import path
 import configparser
 
 
@@ -33,98 +34,143 @@ def discogsPull(user):
             for info in release:
                 if type(release[info])== dict:
                     library[release[info]['title']] = release[info]['artists'][0]['name']
-        libraryKeys = list(library)
-        return library, libraryKeys
+        return library
 
-def getRandom(recAmt,library,libraryKeys):
+def genSuggest(recAmt,library,libraryKeys):
     """
     the user must be valid, parsing through data and populating a list of their collection
     if we were successfully able to pull all of this information, open a new config and write that username to file 
     """
     random.seed()
-    listenTo = []
+    listenList = []
     for ct in range(recAmt):
         tempRec = libraryKeys[random.randrange(len(library))]
         tempStr = tempRec+' by '+library[tempRec]
-        if tempStr not in listenTo:
-            listenTo.append(tempStr)
+        if tempStr not in listenList:
+            listenList.append(tempStr)
         else:
             ct += 1
-    return listenTo
+    return listenList
 
-def popLibrary(user,recAmt):
+
+def popLibrary(user):
     """
     does the heavy lifting, interprates discogsPull and prints the recommendations.
     """
     if discogsPull(user) != False:
-        library, libraryKeys = discogsPull(user)
-        listenTo = getRandom(recAmt,library,libraryKeys)
-        print('\nYou should listen to:\n')
-        for rec in listenTo:
-            print(rec)
-        # for ct in range(0,recAmt):
-        #     print(listenTo,'by',library[listenTo])
-        #     listenTo = getRandom(library,libraryKeys)
+        library = discogsPull(user)
+        return library
 
     else:
         print('Invalid User "{}".'.format(user))
         user = input('Enter discogs username:')
         popLibrary(user)
 
-# def configParse(toParse):
     
-
-
-home = getenv("HOME") # defining home folder
-config = configparser.ConfigParser() #initializing config parser object
-config.read('{}/.config/silt.ini'.format(home)) #sending default config file to config parser object
-
-"""
-main-ish
-interprates arguments and decides what to do depending on what arguments are provided
-too messy, I'd like to clean this up
-"""
-if len(argv) > 1:
-    if '-u' in argv:
-        if argv.index('-u') + 1 < len(argv):
-            user = argv[argv.index('-u') + 1]
-        elif not argv.index('-u') + 1 < len(argv):
-            print('Incorrect formatting for "-u" argument. We need something after it.')
-            user = input('Enter discogs username:')
-    elif '-u' not in argv:
-        if 'user' in config['default']:
-            user = config['default']['user']
-        else:
-            user = input('Enter discogs username:')
-
-    if '-r' in argv:
-        if argv.index('-r') + 1 < len(argv):
-            try:
-                int(argv[argv.index('-r') + 1])
-                recAmt = int(argv[argv.index('-r') + 1])
-            except (TypeError,ValueError):
-                print('Incorrect formatting for "-r" argument. We need an integer after it.')
-                help()
-                recAmt = 0
-        else:
-            help()
-    else:
-        if 'user' in config['default']:
-            user = config['default']['user']
-        else:
-            user = input('Enter discogs username:')
-        help()
-
-else: 
-    recAmt = 1
-    if 'recAmt' in config['default']:
-        recAmt = config['default']
-    else:
-        recAmt = 1
-
+def configParse():
+    home = getenv("HOME") # defining home folder
+    config = configparser.ConfigParser() #initializing config parser object
+    config.read('{}/.silt/silt.ini'.format(home)) #sending default config file to config parser object
+    user,recAmt,library,libraryFile = None,None,None,None
     if 'user' in config['default']:
         user = config['default']['user']
-    else:
-        user = input('Enter discogs username:')
+    if 'recAmt' in config['default']:
+        try:
+            recAmt = int(config['default']['recAmt'])
+        except (TypeError,ValueError):
+            recAmt = None
+    if 'libraryFile' in config['default']:
+        libraryFile = config['default']['libraryFile']
+        libraryFile = path.expanduser(libraryFile)
+        library = libraryFromFile(libraryFile)
+    elif 'libraryFile' not in config['default']:
+        libraryFile = 'libraryFile.json'
 
-popLibrary(user,recAmt)
+    return user,recAmt,library,libraryFile
+
+def libraryFromFile(libraryFile):
+    try:
+        with open(libraryFile,'r') as database:
+            library = json.load(database)
+            return library
+
+    except ( FileNotFoundError ):
+        return None
+
+
+def libraryDump(library,libraryFile):
+    try:
+        with open(libraryFile,'w+') as database:
+            json.dump(library,database)
+    except:
+        home = getenv("HOME")
+        libraryFile = '{}/.silt/libraryFile.json'.format(home)
+        with open(libraryFile,'w+') as database:
+            json.dump(library,database)
+
+
+def main():
+    user,recAmt,library,libraryFile = configParse()
+    if user == None:
+        user = input('Enter discogs username:')
+    if recAmt == None:
+        recAmt = 1
+
+    """
+    main-ish
+    interprates arguments and decides what to do depending on what arguments are provided
+    too messy, I'd like to clean this up
+    """
+    validArgs = ['-u','-r','-h','-update']
+    if len(argv) > 1:
+        if '-u' in argv:
+            if argv.index('-u') + 1 < len(argv):
+                user = argv[argv.index('-u') + 1]
+            elif not argv.index('-u') + 1 < len(argv): # no username given
+                print('Incorrect formatting for "-u" argument. We need a number after it.')
+                user = input('Enter discogs username:')
+
+        if '-r' in argv:
+            if argv.index('-r') + 1 < len(argv):
+                try:
+                    int(argv[argv.index('-r') + 1])
+                    recAmt = int(argv[argv.index('-r') + 1])
+
+                except (TypeError,ValueError):
+                    print('Incorrect formatting for "-r" argument. We need an integer after it. Defaulting to 1')
+                    help()
+                    recAmt = 1
+            else:
+                print("Incorrect formatting for "-r" argument. We need a number after it.")
+                recAmt = 1
+                help()
+
+        if '-h' in argv:
+            help()
+
+        if '-update' in argv:
+            print('Updating...')
+            library = popLibrary(user)
+            libraryDump(library,libraryFile)
+
+        if '-u' not in argv and '-r' not in argv and '-update' not in argv: # some argument was given but not valid
+            help()
+
+    if library == None:
+        library = popLibrary(user)
+        libraryDump(library,libraryFile)
+        listenList = genSuggest(recAmt,library,list(library))
+        print('\nYou should listen to:\n')
+        for rec in listenList:
+            print(rec)
+
+    else:
+        listenList = genSuggest(recAmt,library,list(library))
+        print('\nYou should listen to:\n')
+        for rec in listenList:
+            print(rec)
+
+try:
+    main()
+except KeyboardInterrupt:
+    pass
